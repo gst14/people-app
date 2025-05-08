1076af1b5cf24cfb196e42a1719896899fa7a256
Gaston Avila <gastonavila14@gmail.com>
2025-05-08 15:46:34 -0300

    Color Logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,7 +15,7 @@ function classNames(...classes: string[]) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-cyan-950">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -29,12 +29,7 @@ export default function Navbar() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
-              {/* <img
-                alt="Your Company"
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                className="h-8 w-auto"
-              /> */}
-              <span className={firaCode.variable}>JBK</span><span>People</span>
+              <span className='text-lime-400'><span className={firaCode.variable}>JBK</span><span className='text-white'>People</span></span>
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
